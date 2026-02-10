7c93de88b94171e43806011c4f83ceb10737457c
PrasadHelaskar <126747316+PrasadHelaskar@users.noreply.github.com>
2026-02-10 16:26:07 +0530

    updated resolve path for filenotfound issue fix

--- utils/json_operations.py
+++ utils/json_operations.py
@@ -6,7 +6,11 @@ from dotenv import load_dotenv
 class json_operations():
     def __init__(self,filename=None):
         self.filename= filename
-        self.__private_file_path=resolve_path(os.getenv("LOCATOR_BASE_PATH")+f"{self.filename}.json")
+        base = os.getenv("LOCATOR_BASE_PATH")
+        relative_path = Path(base) / f"{self.filename}.json"
+        self.__private_file_path = resolve_path(relative_path)
+        
+        # self.__private_file_path=resolve_path(os.getenv("LOCATOR_BASE_PATH")+f"{self.filename}.json")
 
     def json_read(self):
         """
